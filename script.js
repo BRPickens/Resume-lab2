4316dc0dfa9cacc7a8ffc47111226f61c398bc00
BRPickens <brpickens@gmail.com>
2017-09-25 17:59:13 -0500

    Everything

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.carousel.carousel-slider').carousel();
+  $('.parallax').parallax();
+  $(".button-collapse").sideNav();
+});
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
